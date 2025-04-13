f5af56dd373eda2df7c9389880ac1f7299c240bb
prxpostern <44132439+prxpostern@users.noreply.github.com>
2025-04-13 09:01:47 +0330

    Update custom_thumbnail.py

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -50,13 +50,13 @@ async def generate_custom_thumbnail(bot, update):
                         chat_id=update.chat.id,
                         photo=save_final_image,
                         caption=Translation.CUSTOM_CAPTION_UL_FILE.format(mention),
-                        reply_to_message_id=update.message_id
+                        reply_to_message_id=update.id
                     )
                 else:
                     await bot.send_message(
                         chat_id=update.chat.id,
                         text=Translation.ERR_ONLY_TWO_MEDIA_IN_ALBUM,
-                        reply_to_message_id=update.message_id
+                        reply_to_message_id=update.id
                     )
                 try:
                     [os.remove(download_location + i) for i in list_im ]
@@ -67,13 +67,13 @@ async def generate_custom_thumbnail(bot, update):
                 await bot.send_message(
                     chat_id=update.chat.id,
                     text=Translation.REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB,
-                    reply_to_message_id=update.message_id
+                    reply_to_message_id=update.id
                 )
         else:
             await bot.send_message(
                 chat_id=update.chat.id,
                 text=Translation.REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB,
-                reply_to_message_id=update.message_id
+                reply_to_message_id=update.id
             )
 
 
@@ -100,7 +100,7 @@ async def save_photo(bot, update):
             await bot.send_message(
                 chat_id=update.chat.id,
                 text=Translation.SAVED_CUSTOM_THUMB_NAIL,
-                reply_to_message_id=update.message_id
+                reply_to_message_id=update.id
             )
 
 
@@ -116,5 +116,5 @@ async def delthumbnail(bot, update):
         await bot.send_message(
             chat_id=update.chat.id,
             text=Translation.DEL_ETED_CUSTOM_THUMB_NAIL,
-            reply_to_message_id=update.message_id
+            reply_to_message_id=update.id
         )
