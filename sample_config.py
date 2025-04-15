a11212c037fc6e12400f5847a2cc50ab619fc426
prxpostern <44132439+prxpostern@users.noreply.github.com>
2025-04-15 08:20:54 +0330

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -8,7 +8,7 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "626187634 742920327 108735187 390447712").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
